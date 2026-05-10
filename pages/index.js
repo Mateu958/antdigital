ed27bebdadb316eacc1cf38e68bd0ca55f4273d4
Mateu958 <ant.cscomercial@gmail.com>
2026-05-10 15:03:11 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,63 @@
+import { useState } from "react"
+
 export default function Home() {
+
+  const [email, setEmail] = useState("")
+  const [senha, setSenha] = useState("")
+
+  function entrar() {
+
+    if(email === "admin@gmail.com" && senha === "123456") {
+      alert("Login realizado com sucesso")
+    } else {
+      alert("Email ou senha incorretos")
+    }
+
+  }
+
   return (
     <div style={{ padding: 20 }}>
+
       <h1>Tela de Login ANT</h1>
 
       <input
         type="email"
         placeholder="Email"
-        style={{ display: "block", marginBottom: 10 }}
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        style={{
+          display: "block",
+          marginBottom: 10,
+          padding: 10,
+          width: 250
+        }}
       />
 
       <input
         type="password"
         placeholder="Senha"
-        style={{ display: "block", marginBottom: 10 }}
+        value={senha}
+        onChange={(e) => setSenha(e.target.value)}
+        style={{
+          display: "block",
+          marginBottom: 10,
+          padding: 10,
+          width: 250
+        }}
       />
 
-      <button>Entrar</button>
+      <button
+        onClick={entrar}
+        style={{
+          padding: 10,
+          background: "green",
+          color: "white",
+          border: "none"
+        }}
+      >
+        Entrar
+      </button>
+
     </div>
   )
 }
