04a1602c5ec13aea8dbb1b81df7ef28274ef5d91
Mateu958 <ant.cscomercial@gmail.com>
2026-05-10 13:33:26 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Sistema de microcrédito</h1>
-      <p>Projeto funcionando no Vercel.</p>
+    <div style={{padding: 20}}>
+      <h1>Sistema de Microcrédito ANT</h1>
+      <p>Projeto online com sucesso.</p>
+
+      <button style={{
+        padding: 10,
+        background: "green",
+        color: "white",
+        border: "none",
+        borderRadius: 5
+      }}>
+        Entrar
+      </button>
     </div>
   )
 }
